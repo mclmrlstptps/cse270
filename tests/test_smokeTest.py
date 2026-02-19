031c2f780a9273bff1163760f136c7527bde670d
Micaela Tapia <mclmrls29@gmail.com>
2026-02-18 20:19:52 -0500

    headless mode

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -80,6 +80,4 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "submit").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "email")))
     self.driver.find_element(By.NAME, "email").click()
-    WebDriverWait(self.driver, 30).until(expected_conditions.element_to_be_clickable((By.NAME, "email")))
-    self.driver.find_element(By.NAME, "email").click()
   
